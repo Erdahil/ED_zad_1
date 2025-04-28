97c176d0a69ab04fa1967a0ee5cb5172ae5170e9
Paweł Charysz <pawel.erdahil@gmail.com>
2025-04-28 17:39:05 +0200

    outfixed deploy

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/ED_zad_1/',
-  plugins: [react()],
-})
